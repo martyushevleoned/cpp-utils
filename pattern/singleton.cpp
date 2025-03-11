fc14e804443014027a19004b8e831943cb40852c
Martyushev Leoned <lyonya.martyushev@mail.ru>
2025-03-12 00:39:30 +0500

    singleton

---
+++ pattern/singleton.cpp
@@ -0,0 +1,17 @@
+class Singleton
+{
+
+public:
+    static Singleton &getInstance()
+    {
+        static Singleton instance;
+        return instance;
+    };
+
+private:
+    Singleton() {};
+
+    Singleton(Singleton const &) = delete;
+
+    void operator=(Singleton const &) = delete;
+};
